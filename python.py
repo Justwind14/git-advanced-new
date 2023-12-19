a6989ba24344296023333db6104fd8ab42accc4a
Justwind14 <justwind14@yandex.ru>
2023-12-19 22:05:27 +0300

    one more function added

--- python.py
+++ python.py
@@ -10,3 +10,35 @@ def binary_search(someList: list) -> function:
 
     :return: Возвращает результат внутренней функции binary_search_helper
     """
+
+    def binary_search_helper(someList: list, number: int) -> int:
+        """Внутренняя функция, хранящая в себе алгоритм бинарного поиска.
+
+        :params:
+            - someList: массив чисел
+            - искомое число
+
+        :type someList: list
+        :tyoe number: int
+
+        :return: Возвращает искомое число, если он присутствует в масиве, либо -1, если такого числа в принимаемом массиве нет
+        """
+
+        # создаем переменную, хранящую в себе индекс среднего элемента массива
+        delimiter = len(someList) // 2
+
+        if someList[delimiter] > number:
+            return binary_search_helper(someList[:delimiter], number)
+        if someList[delimiter] < number:
+            return binary_search_helper(someList[delimiter:], number)
+        if someList[delimiter] == number:
+            return someList[delimiter]
+        else:
+            return -1
+
+    if len(someList) != 0:
+        # возвращаем результат работы внутренней функции с искомым параметром number
+        return lambda number: binary_search_helper(someList, number)
+    else:
+        # если массив пустой, вызов функции ничего не вернет
+        return None
