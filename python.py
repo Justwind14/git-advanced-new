e3f9aa274f28ed0e2e8195d0bd882ea2e1919bff
Justwind14 <justwind14@yandex.ru>
2023-12-19 23:35:05 +0300

    rebase

--- python.py
+++ python.py
@@ -42,6 +42,37 @@ def binary_search(someList: list) -> function:
     else:
         # если массив пустой, вызов функции ничего не вернет
         return None
+    def binary_search_helper(someList: list, number: int) -> int:
+        """Внутренняя функция, хранящая в себе алгоритм бинарного поиска.
+
+        :params:
+            - someList: массив чисел
+            - искомое число
+
+        :type someList: list
+        :tyoe number: int
+
+        :return: Возвращает искомое число, если он присутствует в масиве, либо -1, если такого числа в принимаемом массиве нет
+        """
+
+        # создаем переменную, хранящую в себе индекс среднего элемента массива
+        delimiter = len(someList) // 2
+
+        if someList[delimiter] > number:
+            return binary_search_helper(someList[:delimiter], number)
+        if someList[delimiter] < number:
+            return binary_search_helper(someList[delimiter:], number)
+        if someList[delimiter] == number:
+            return someList[delimiter]
+        else:
+            return -1
+
+    if len(someList) != 0:
+        # возвращаем результат работы внутренней функции с искомым параметром number
+        return lambda number: binary_search_helper(someList, number)
+    else:
+        # если массив пустой, вызов функции ничего не вернет
+        return None
     
 
 print('oh my God')
