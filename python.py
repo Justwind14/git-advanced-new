875e509611b56a264b86d561f42bb9222231c985
Justwind14 <justwind14@yandex.ru>
2023-12-19 22:04:19 +0300

    new function added

--- python.py
+++ python.py
@@ -1,1 +1,12 @@
-print('hey')
+print("hey")
+
+def binary_search(someList: list) -> function:
+    """Функция поиска числа в массиве чисел.
+
+    :params:
+        - someList: массив чисел
+
+    :type someList: list
+
+    :return: Возвращает результат внутренней функции binary_search_helper
+    """
